f07043ddc9d30462c0f4a15f2edd618f9ad8d26c
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2016-03-15 23:26:12 +0100

    Proof of concept playing on sonos

--- src/sonopod.py
+++ src/sonopod.py
@@ -24,10 +24,25 @@ class SimplePodcasts(object):
             self.episodes =  [Episode(e['title'], e['description'], e['enclosures'][0]['url']) for e in pc['episodes']]
         return self.episodes
 
+
+class SonosPlayer(object):
+    def __init__(self):
+        self.players = soco.discover()
+        self.default = list(self.players)[0]
+
+    def play(self, episode):
+        logging.debug('Playing episode %r on %r', episode, self.default)
+        self.default.play_uri(uri=episode.url,
+                              #meta= , # DIDL format
+                              title=episode.title,
+                              start=True)
+
 if __name__=='__main__':
     logging.basicConfig(level=logging.DEBUG)
     import sys
     import argparse
+    from builtins import input
+
     parser = argparse.ArgumentParser(
         description='Play podcasts on you Sonos from the command line')
     parser.add_argument('podcasturl', help='The url of the podcast')
@@ -41,11 +56,14 @@ if __name__=='__main__':
     for (idx,ep) in enumerate(eps, start=1):
         print('[{}]\t {} - {} '.format(idx, ep.title, ep.url))
 
+    playthis = -1
+    while not 0 < playthis < len(eps):
+        try:
+            playthis = int(input('Which episode to play> '))-1 # deduct 1 b/c zero indexing
+        except ValueError:
+            pass
+    logging.debug('Got episode index %r from user input', playthis)
 
-
-
-    
-    
-    
-
+    player = SonosPlayer()
+    player.play(eps[playthis])
 
