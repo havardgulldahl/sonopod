0e4b025a0dec9e933d6999e471b4abc3382dbc48
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2016-03-16 12:13:03 +0100

    add metadata

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+""" sonopod is a commandline tool for playing podcasts through Sonos speakers """
+
+# Will be parsed by setup.py to determine package metadata
+__author__ = '<havard@gulldahl.no>'
+__version__ = '0.1'
+__website__ = 'https://github.com/havardgulldahl/sonopod
+__license__ = 'GPLv3 License'
+
