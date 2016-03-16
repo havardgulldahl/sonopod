9cd71bbdd217e710ad52fe7852252aa6ee70fcff
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2016-03-16 21:52:12 +0100

    require setup tools

--- setup.py
+++ setup.py
@@ -1,53 +1,35 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup
-    has_setuptools = True
-except ImportError:
-    from distutils.core import setup
-    has_setuptools = False
+""" sonopod is a commandline tool for playing podcasts through Sonos speakers """
+
+# Will be parsed by setup.py to determine package metadata
+__author__ = '<havard@gulldahl.no>'
+__version__ = '0.1.1'
+__website__ = 'https://github.com/havardgulldahl/sonopod'
+__license__ = 'GPLv3 License'
+
+from setuptools import setup
 
 import re
 import os.path
 
-dirname = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(dirname, 'sonopod/__init__.py')
-src = open(filename).read()
-metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", src))
-docstrings = re.findall('"""(.*)"""', src)
-
-NAME = 'sonopod'
-PACKAGES = [ 'sonopod', ]
-
-AUTHOR_EMAIL = metadata['author']
-VERSION = metadata['version']
-WEBSITE = metadata['website']
-LICENSE = metadata['license']
-DESCRIPTION = docstrings[0]
-
 REQUIREMENTS = list(open('requirements.txt'))
 
-OPTIONS = {}
+OPTIONS = {
+    'install_requires': REQUIREMENTS,
+}
 
-if has_setuptools:
-    OPTIONS = {
-        'install_requires': REQUIREMENTS,
-    }
-
-# Extract name and e-mail ("<mail@example.org>")
-EMAIL, = re.match(r'<(.*)>', AUTHOR_EMAIL).groups()
-
-setup(name=NAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      author=EMAIL,
-      author_email=EMAIL,
-      license=LICENSE,
-      url=WEBSITE,
-      packages=PACKAGES,
+setup(name='sonopod',
+      version=__version__,
+      description= """ sonopod is a commandline tool for playing podcasts through Sonos speakers """,
+      author=__author__,
+      author_email=__author__,
+      license=__license__,
+      url=__website__,
+      #scripts=['sonopod.py'],
       entry_points={
           'console_scripts': [
-              'sonopod = sonopod.sonopod:main',
+              'sonopod = sonopod:main',
           ]
       },
       **OPTIONS)
