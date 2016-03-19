79d4f316124b03c21b55ee7993fc3ba1f64aba45
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2016-03-20 00:58:42 +0100

    Getting ready to release 0.1.1

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ import os.path
 setup(name='sonopod',
       version=__version__,
       description= """ sonopod is a commandline tool for playing podcasts through Sonos speakers """,
+      long_description=open('README').read(),
       author=__author__,
       author_email=__author__,
       license=__license__,
